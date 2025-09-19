e53c0e54af4ac0a5018c5924c1fef21380298418
Valerie2009 <57894801+Valerie2009@users.noreply.github.com>
2025-09-19 12:44:35 +0200

    Code mit erklärenden Kommentaren

---
+++ a01_helper.py
@@ -0,0 +1,141 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.7
+# ---
+
+# %%
+import psutil
+import numpy as np
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+from mnist import MNIST  # run from Anaconda shell: pip install python-mnist
+
+# %% [markdown]
+# # Datasets
+
+# %%
+mndata = MNIST("data/")
+X, y = mndata.load_training()
+y = np.array(y, dtype="uint8")
+X = np.array([np.array(x) for x in X], dtype="uint8")
+N, D = X.shape  # bestimmt die Dimensionen von X : N sind die Anzahl Zeilen und D die Anzahl Spalten => N=60000 , D= 784
+Xtest, ytest = mndata.load_testing()
+ytest = np.array(ytest, dtype="uint8")
+Xtest = np.array([np.array(x) for x in Xtest], dtype="uint8")
+Ntest = Xtest.shape[0] # Anzahl an Zeilen im Testdatensatz, hier 10000 Zeilen
+
+# %%
+# in dem folgenden Abschnitt wird einen kleinerer Trainingsdatensatz mit 100 Beispielen 
+# und ein Testdatensatz mit 10 Beispielen erstellt
+p = np.zeros(0, dtype="int")
+for c in range(10): # für jede Ziffer 0 bis 9 werden zufällig 100 Datenpunkte aus dem Trainingsdatensatz ausgewählt 
+    p = np.append(p, np.random.choice(np.where(y == c)[0], size=100, replace=False))
+X_s = X[p, :] # mit den eben zufällig ausgewählten 100 Beispielen wird ein neuer Trainingsdatensatz erstellt
+y_s = y[p]
+N_s = X_s.shape[0]
+p = np.zeros(0, dtype="int")
+for c in range(10):
+    p = np.append(p, np.random.choice(np.where(ytest == c)[0], size=10, replace=False))
+Xtest_s = Xtest[p, :]
+ytest_s = ytest[p]
+Ntest_s = Xtest_s.shape[0]
+
+# %%
+# A simple example dataset that you can use for testing
+Xex = np.array([1, 0, 0, 1, 1, 1, 2, 0]).reshape(4, 2)
+yex = np.array([0, 1, 2, 0])
+
+# %% [markdown]
+# # Plotting
+
+# %%
+# der folgende Abschnitt ist nur zur Überprüfung ob der Code in Jypiter Notebook oder sonst wo ausgeführt wird
+# und je nachdem wird dann die entsprechende Bibliothek zur Darstellung von Diagrammen ausgesucht
+
+inTerminal = not "IPKernelApp" in get_ipython().config
+inJupyterNb = any(
+    filter(
+        lambda x: x.endswith("jupyter-notebook"), psutil.Process().parent().cmdline()
+    )
+)
+inJupyterLab = any(
+    filter(lambda x: x.endswith("jupyter-lab"), psutil.Process().parent().cmdline())
+)
+if not inJupyterLab:
+    from IPython import get_ipython
+
+    get_ipython().run_line_magic(
+        "matplotlib", "" if inTerminal else "notebook" if inJupyterNb else "widget"
+    )
+
+
+
+# %%
+# Vorbereitung zum plotten von Diagrammen indem eventuell noch vorhandende Bilder gelöscht werden 
+# oder schon eine Vorlage erstellt wird
+def nextplot():
+    if inTerminal:
+        plt.clf()  # this clears the current plot
+    else:
+        plt.figure()  # this creates a new plot
+
+
+
+# %%
+def showdigit(x):
+    "Show one digit as a gray-scale image."
+    plt.imshow(x.reshape(28, 28), norm=mpl.colors.Normalize(0, 255), cmap="gray")
+
+
+# %%
+def showdigits(X, y, max_digits=15):
+    "Show up to max_digits random digits per class from X with class labels from y."
+    num_cols = min(max_digits, max(np.bincount(y)))
+    for c in range(10):
+        ii = np.where(y == c)[0]
+        if len(ii) > max_digits:
+            ii = np.random.choice(ii, size=max_digits, replace=False)
+        for j in range(num_cols):
+            ax = plt.gcf().add_subplot(
+                10, num_cols, c * num_cols + j + 1, aspect="equal"
+            )
+            ax.get_xaxis().set_visible(False)
+            if j == 0:
+                ax.set_ylabel(c)
+                ax.set_yticks([])
+            else:
+                ax.get_yaxis().set_visible(False)
+            if j < len(ii):
+                ax.imshow(
+                    X[ii[j],].reshape(28, 28),
+                    norm=mpl.colors.Normalize(0, 255),
+                    cmap="gray",
+                )
+            else:
+                ax.axis("off")
+
+
+# %% [markdown]
+# # Training
+
+# %%
+def logsumexp(x):
+    """Computes log(sum(exp(x)).
+
+    Uses offset trick to reduce risk of numeric over- or underflow. When x is a
+    1D ndarray, computes logsumexp of its entries. When x is a 2D ndarray,
+    computes logsumexp of each column.
+
+    Keyword arguments:
+    x : a 1D or 2D ndarray
+    """
+    offset = np.max(x, axis=0)
+    return offset + np.log(np.sum(np.exp(x - offset), axis=0))
